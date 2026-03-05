aa71959fec1628d170a1dd55f305a43a60ef4d79
Stanley Wintergreen <kepocnhh@protonmail.com>
2026-03-05 21:07:53 +0300

    wip

--- app/src/main/kotlin/test/android/animated/MainActivity.kt
+++ app/src/main/kotlin/test/android/animated/MainActivity.kt
@@ -2,10 +2,13 @@ package test.android.animated
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.compose.ui.platform.ComposeView
 
 internal class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // todo
+        val view = ComposeView(this)
+        setContentView(view)
+        view.setContent(::MainScreen)
     }
 }
---
+++ app/src/main/kotlin/test/android/animated/MainScreen.kt
@@ -0,0 +1,206 @@
+package test.android.animated
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.AnimationSpec
+import androidx.compose.animation.core.Easing
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.VectorConverter
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.text.BasicText
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.withFrameNanos
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import java.util.concurrent.atomic.AtomicReference
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.seconds
+
+@Composable
+private fun v0() {
+    AnimatedVisibility(
+        visible = true,
+    ) {
+        // todo
+    }
+}
+
+@Composable
+private fun v1(
+    initialValue: Float,
+    targetValue: Float,
+    duration: Duration,
+    easing: Easing,
+): Float {
+    val animatable = remember {
+        Animatable(initialValue, Float.VectorConverter, null, "foobarbaz")
+    }
+    LaunchedEffect(targetValue) {
+        animatable.animateTo(targetValue, tween(durationMillis = duration.inWholeMilliseconds.toInt(), easing = easing))
+    }
+    return animatable.value
+}
+
+@Composable
+private fun animatedFloat(
+    initialValue: Float,
+    targetValue: Float,
+    duration: Duration,
+    easing: Easing,
+): Float {
+    val values = remember { mutableFloatStateOf(initialValue) }
+    val targetValues = remember { AtomicReference(targetValue) }
+    val fractions = remember { AtomicReference(0f) }
+    LaunchedEffect(targetValue) {
+        targetValues.set(targetValue)
+        val message = """
+            target: $targetValue
+            value: ${values.floatValue}
+        """.trimIndent()
+        println(message)
+        val timeNanos = duration.inWholeNanoseconds.toFloat()
+//        val timeStart = withFrameNanos { it }
+        val timeStart = System.nanoTime() // todo
+        val appendix = fractions.getAndSet(0f)
+        val startedValue = values.floatValue
+        val valuesDiff = targetValue - startedValue
+        if (valuesDiff != 0f) {
+            withContext(Dispatchers.Default) {
+                while (true) {
+                    if (targetValue != targetValues.get()) {
+                        println("new target") // todo
+                        break
+                    }
+//                val timeDiff = withFrameNanos { it - timeStart }.toFloat()
+                    val timeNow = System.nanoTime()
+                    val timeDiff = timeNow.minus(timeStart).toFloat()
+                    if (timeDiff < timeNanos) {
+                        val fraction = timeDiff / timeNanos
+                        values.floatValue = startedValue + valuesDiff * easing.transform(fraction)
+                        fractions.set(fraction)
+                    } else {
+                        values.floatValue = targetValue
+                        fractions.set(0f)
+                        break
+                    }
+                }
+            }
+        }
+    }
+    return values.floatValue
+}
+
+@Composable
+internal fun MainScreen() {
+    val targetValues = remember { mutableFloatStateOf(0f) }
+    val currentValue = animatedFloat(
+//    val currentValue = v1(
+        initialValue = 0f,
+        targetValue = targetValues.value,
+        duration = 2.seconds,
+        easing = LinearEasing,
+//        easing = FastOutSlowInEasing,
+    )
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.White),
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.Center),
+        ) {
+            val text = """
+                current: $currentValue
+                target: ${targetValues.floatValue}
+            """.trimIndent()
+            BasicText(
+                modifier = Modifier.fillMaxWidth().padding(16.dp),
+                text = text,
+            )
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(64.dp),
+            ) {
+                Spacer(
+                    modifier = Modifier
+                        .fillMaxHeight()
+                        .fillMaxWidth(fraction = currentValue)
+                        .background(color = Color.Red),
+                )
+            }
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+            ) {
+                BasicText(
+                    modifier = Modifier
+                        .height(48.dp)
+                        .weight(1f)
+                        .clickable {
+                            targetValues.value = 0f
+                        }
+                        .wrapContentSize(),
+                    text = "set target 0",
+                )
+                BasicText(
+                    modifier = Modifier
+                        .height(48.dp)
+                        .weight(1f)
+                        .clickable {
+                            targetValues.value = 1f
+                        }
+                        .wrapContentSize(),
+                    text = "set target 1",
+                )
+            }
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+            ) {
+                BasicText(
+                    modifier = Modifier
+                        .height(48.dp)
+                        .weight(1f)
+                        .clickable {
+                            targetValues.value = 0.25f
+                        }
+                        .wrapContentSize(),
+                    text = "set target 0.25",
+                )
+                BasicText(
+                    modifier = Modifier
+                        .height(48.dp)
+                        .weight(1f)
+                        .clickable {
+                            targetValues.value = 0.75f
+                        }
+                        .wrapContentSize(),
+                    text = "set target 0.75",
+                )
+            }
+        }
+    }
+}
